f544916401402576b4ea82aba2c5971f4868c6ec
ildarjan <nobap84@rambler.ru>
2021-06-05 00:38:40 +0300

    use new components

--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import {randomElem, randomInt} from './utils.js';
+import {GENRES, TITLES} from './consts.js';
+
+function getFilmCardProps () {
+  return {
+    title: randomElem(TITLES),
+    genre: randomElem(GENRES),
+    year: randomInt(2021, 2000),
+  };
+}
+
+const cardFilmProps = getFilmCardProps();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={cardFilmProps.title}
+      genre={cardFilmProps.genre}
+      year={cardFilmProps.year}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
