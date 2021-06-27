e99328a864856a621a1b8c790e760644c60ab7a0
ildarjan <nobap84@rambler.ru>
2021-06-27 20:01:19 +0300

    fix naming, use new action

--- project/src/store/reducer.js
+++ project/src/store/reducer.js
@@ -1,24 +1,21 @@
 import {DEFAULT_GENRE_TYPE} from '../const.js';
 import {ActionType} from './action.js';
 import {extend} from '../utils.js';
-import {films} from '../mocks/films.js';
 import {getFilmsLists} from './store-utils.js';
 
-const [filmsById, filmsByGenre] = getFilmsLists(films);
-
 const initialState = {
-  genre: DEFAULT_GENRE_TYPE,
-  filmsList: filmsById,
-  filmsByGenre: filmsByGenre,
+  currentGenre: DEFAULT_GENRE_TYPE,
+  filmsList: [],
+  filmsByGenre: {},
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case (ActionType.CHANGE_GENRE):
-      return extend(state, {genre: action.payload});
-    case (ActionType.GET_FILMS): {
-      const [filmsListById, filmsListByGenre] = getFilmsLists(action.payload);
-      return extend(state, {filmsById: filmsListById, filmsByGenre: filmsListByGenre});
+    case (ActionType.CHANGE_CURRENT_GENRE):
+      return extend(state, {currentGenre: action.payload});
+    case (ActionType.FILMS_LOADING_SUCCESS): {
+      const [filmsList, filmsByGenre] = getFilmsLists(action.payload);
+      return extend(state, {filmsList, filmsByGenre});
     }
   }
   return state;
