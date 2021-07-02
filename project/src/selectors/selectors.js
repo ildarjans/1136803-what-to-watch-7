e4c7a7de5a33f2ea45d1f96037f24f110761adde
ildarjan <nobap84@rambler.ru>
2021-07-02 22:20:25 +0300

    add selectors

--- project/src/selectors/selectors.js
+++ project/src/selectors/selectors.js
@@ -1,6 +1,18 @@
 
-export const selectRouteId = (props) => Number(props.match.params.id);
+export const selectNormalizedFilmId = (props) => Number(props.id);
+
+export const selectFilmIdFromRoute = (props) => Number(props.match.params.id);
 
 export const selectFilmsByGenre = ({filmsByGenre, currentGenre}) => filmsByGenre[currentGenre];
 
-export const selectFilmById = ({filmsList}, props) => filmsList[selectRouteId(props)];
+export const selectFilmById = ({filmsList}, id) => filmsList[id];
+
+export const selectFavoriteList = ({favoriteList}) => favoriteList;
+
+export const selectAuthorizationStatus = ({authorizationStatus}) => authorizationStatus;
+
+export const selectUser = ({user}) => user;
+
+export const selectCurrentGenre = ({currentGenre}) => currentGenre;
+
+export const selectAllGenres = ({filmsByGenre}) => Object.keys(filmsByGenre);
