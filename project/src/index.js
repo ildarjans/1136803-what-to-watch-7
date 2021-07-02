012460c3dcda61d167861dccca15a1a712ef1683
ildarjan <nobap84@rambler.ru>
2021-07-02 22:32:31 +0300

    add middleware and async requests before start app

--- project/src/index.js
+++ project/src/index.js
@@ -3,24 +3,32 @@ import ReactDOM from 'react-dom';
 import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import App from './components/app/app';
 import {reducer} from './store/reducer.js';
-import thunk from 'redux-thunk';
 import createAPI from './services/api.js';
-import {fetchFilms} from './middleware/thunk-api.js';
+import {checkAuthUser, fetchFavorites, fetchFilms} from './middleware/thunk-api.js';
+import {redirect} from './middleware/redirect.js';
+import {ActionCreator} from './store/action.js';
+import {AuthStatus} from './const.js';
 
-const api = createAPI(() => {
-});
+const api = createAPI(() => (
+  store.dispatch(ActionCreator.setAuthorizationStatus(AuthStatus.NO_AUTHORIZED))),
+);
 
 const store = createStore(
   reducer,
   composeWithDevTools(
     applyMiddleware(thunk.withExtraArgument(api)),
+    applyMiddleware(redirect),
   ),
 );
 
-Promise
-  .resolve(store.dispatch(fetchFilms()))
+Promise.all([
+  store.dispatch(checkAuthUser()),
+  store.dispatch(fetchFilms()),
+  store.dispatch(fetchFavorites()),
+])
   .then(() => {
     ReactDOM.render(
       <Provider store={store}>
