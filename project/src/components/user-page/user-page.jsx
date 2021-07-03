a7a818fb47ffeb70cb3c8da9c2c7a1ba6eb602a4
ildarjan <nobap84@rambler.ru>
2021-07-04 01:51:34 +0300

    remove login route

--- project/src/components/user-page/user-page.jsx
+++ project/src/components/user-page/user-page.jsx
@@ -1,33 +1,42 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Footer from '../footer/footer.jsx';
 import Header from '../header/header.jsx';
 import SignInForm from '../sign-in-form/sign-in-form.jsx';
 import {loginUser} from '../../middleware/thunk-api.js';
-import {HeaderClass} from '../../const.js';
+import {selectAuthorizationStatus} from '../../selectors/selectors.js';
+import {AppRoute, AuthStatus, HeaderClass} from '../../const.js';
 
-function UserPage({onSubmit}) {
-  return (
-    <div className="user-page">
+function UserPage({authorizationStatus, onSubmit}) {
+  return authorizationStatus === AuthStatus.NO_AUTHORIZED ?
+    (
+      <div className="user-page">
 
-      <Header className={HeaderClass.USER_PAGE}>
-        <h1 className="page-title user-page__title">Sign in</h1>
-      </Header>
+        <Header className={HeaderClass.USER_PAGE}>
+          <h1 className="page-title user-page__title">Sign in</h1>
+        </Header>
 
-      <div className="sign-in user-page__content">
-        <SignInForm onSubmit={onSubmit}/>
+        <div className="sign-in user-page__content">
+          <SignInForm onSubmit={onSubmit}/>
+        </div>
+
+        <Footer/>
+
       </div>
-
-      <Footer/>
-
-    </div>
-  );
+    ) :
+    <Redirect to={AppRoute.ROOT}/>;
 }
 
 UserPage.propTypes = {
   onSubmit: PropTypes.func.isRequired,
+  authorizationStatus: PropTypes.string.isRequired,
 };
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: selectAuthorizationStatus(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   onSubmit(authInfo) {
@@ -35,4 +44,4 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
-export default connect(null, mapDispatchToProps)(UserPage);
+export default connect(mapStateToProps, mapDispatchToProps)(UserPage);
