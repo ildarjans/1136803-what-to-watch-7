2e1a21fade3431cbbfc347204bd045f951efeaa3
ildarjan <nobap84@rambler.ru>
2021-07-09 23:44:25 +0300

    add fetchSimilarFilms on useEffects

--- project/src/components/movie-page/movie-page.jsx
+++ project/src/components/movie-page/movie-page.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 import {withRouter} from 'react-router-dom';
 import FilmList from '../film-list/film-list.jsx';
 import Footer from '../footer/footer.jsx';
@@ -8,10 +9,16 @@ import FilmCardFull from '../film-card-full/film-card-full.jsx';
 import {getFilmByID, getFilmsMoreLikeThis} from '../../utils.js';
 import {CatalogTitle} from '../../const.js';
 import {filmPropTypes, filmsPropTypes} from '../../prop-types/films.js';
-import {selectFilmIdFromRoute, selectFilmsByGenre} from '../../selectors/selectors.js';
+import {selectAuthorizationStatus, selectFilmIdFromRoute, selectFilmsByGenre} from '../../selectors/selectors.js';
+import {fetchSimilarFilms} from '../../middleware/thunk-api.js';
 
-function MoviePage({films, id}) {
+function MoviePage({films, id, authorizationStatus, fetchSimilarFilmsById, similarFilms}) {
   const film = getFilmByID(films, id);
+
+  useEffect(() => {
+    fetchSimilarFilmsById(id);
+  }, [id]);
+
 
   if (!film) {
     return <NotFoundPage/>;
@@ -19,11 +26,11 @@ function MoviePage({films, id}) {
 
   return (
     <>
-      <FilmCardFull film={film}/>
+      <FilmCardFull film={film} authorizationStatus={authorizationStatus}/>
       <div className="page-content">
         <section className="catalog catalog--like-this">
           <h2 className="catalog__title">{CatalogTitle.MORE_LIKE_THIS}</h2>
-          <FilmList films={getFilmsMoreLikeThis(films, film)}/>
+          <FilmList films={getFilmsMoreLikeThis(similarFilms, film)}/>
         </section>
         <Footer/>
       </div>
@@ -34,11 +41,20 @@ function MoviePage({films, id}) {
 MoviePage.propTypes = {
   id: filmPropTypes.id.isRequired,
   films: filmsPropTypes.isRequired,
+  authorizationStatus: PropTypes.string.isRequired,
+  similarFilms: filmsPropTypes,
+  fetchSimilarFilmsById: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state, ownProps) => ({
   films: selectFilmsByGenre(state),
   id: selectFilmIdFromRoute(ownProps),
+  authorizationStatus: selectAuthorizationStatus(state),
+  similarFilms: state.similarFilms,
 });
 
-export default connect(mapStateToProps)(withRouter(MoviePage));
+const mapDispatchToProps = (dispatch) => ({
+  fetchSimilarFilmsById: (id) => dispatch(fetchSimilarFilms(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(MoviePage));
