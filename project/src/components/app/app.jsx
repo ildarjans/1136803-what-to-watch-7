04c772034bdb6bbef28000ca40e398e42ed0bb6f
ildarjan <nobap84@rambler.ru>
2021-07-10 14:21:06 +0300

    Review name refactor

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -5,7 +5,7 @@ import MoviePage from '../movie-page/movie-page.jsx';
 import MyList from '../my-list/my-list.jsx';
 import UserPage from '../user-page/user-page.jsx';
 import Player from '../player/player.jsx';
-import Review from '../add-review-page/add-review-page.jsx';
+import AddReviewPage from '../add-review-page/add-review-page.jsx';
 import NotFoundPage from '../not-found-page/not-found-page.jsx';
 import {AppRoute} from '../../const.js';
 import {browserHistory} from '../../browser-history.js';
@@ -19,7 +19,7 @@ function App() {
         <Route exact path={AppRoute.LOGIN} component={UserPage}/>
         <Route exact path={AppRoute.FILM} component={MoviePage}/>
         <Route exact path={AppRoute.PLAYER} component={Player}/>
-        <PrivateRoute exact path={AppRoute.REVIEW} render={({match}) => <Review id={match.params.id}/>}/>
+        <PrivateRoute exact path={AppRoute.REVIEW} render={({match}) => <AddReviewPage id={match.params.id}/>}/>
         <PrivateRoute exact path={AppRoute.FAVORITES} render={() => <MyList/>}/>
         <Route component={NotFoundPage}/>
       </Switch>
