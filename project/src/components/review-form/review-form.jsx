e71871953c08cb855f115100698b1003e0d4afea
ildarjan <nobap84@rambler.ru>
2021-07-10 14:27:52 +0300

    add posting state, remove disable fn's and ref's. rename props. refactor

--- project/src/components/review-form/review-form.jsx
+++ project/src/components/review-form/review-form.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 const TEXT_REVIEW_MIN_LENGTH = 50;
@@ -6,10 +6,7 @@ const TEXT_REVIEW_MAX_LENGTH = 400;
 const RATING_MIN_VALUE = 1;
 const RATING_MAX_VALUE = 10;
 
-const FormElementName = {
-  'review-text': 'comment',
-  'rating': 'rating',
-};
+const ratingStars = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const isValidComment = ({length}) => (
   length >= TEXT_REVIEW_MIN_LENGTH && length <= TEXT_REVIEW_MAX_LENGTH
@@ -19,38 +16,25 @@ const isValidRating = (ratingValue) => (
   ratingValue >= RATING_MIN_VALUE && ratingValue <= RATING_MAX_VALUE
 );
 
-const setDisableAttributeForInputElements = (form, isDisable) => {
-  Array.from(form.elements)
-    .forEach((element) => element.disabled = element.tagName === 'INPUT' ? isDisable : element.disabled);
-};
 
-const setDisableAttributeForTextAreaElement = (element, isDisable) => {
-  element.disabled = isDisable;
-};
+function ReviewForm({id, onSubmit, errorMessage}) {
+  const [formData, setFormData] = useState({
+    rating: 0,
+    comment: '',
+  });
+  const [posting, setPosting] = useState(false);
 
-
-const ratingStars = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-
-const initialFormValue = {
-  rating: 0,
-  comment: '',
-};
-
-function ReviewForm({id, onSubmitReviewForm, postErrorMessage}) {
-  const [formData, setFormData] = useState(initialFormValue);
-  const formRef = useRef(null);
-  const textAreaRef = useRef(null);
+  useEffect(() => setPosting(false), [errorMessage]);
 
   const handleInputChange = ({target}) => {
-    setFormData({
-      ...formData, [FormElementName[target.name]]: target.value,
-    });
+    setFormData({...formData, [target.name]: target.value});
   };
 
-  useEffect(() => () => {
-    formRef.current && setDisableAttributeForInputElements(formRef.current, false);
-    textAreaRef.current && setDisableAttributeForTextAreaElement(textAreaRef.current, false);
-  });
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    setPosting(true);
+    onSubmit(id, formData);
+  };
 
   const isValidForm = isValidRating(formData.rating) && isValidComment(formData.comment);
 
@@ -58,14 +42,8 @@ function ReviewForm({id, onSubmitReviewForm, postErrorMessage}) {
     <div className="add-review">
       <form
         action="#"
-        ref={formRef}
         className="add-review__form"
-        onSubmit={(evt) => {
-          evt.preventDefault();
-          setDisableAttributeForInputElements(formRef.current, true);
-          setDisableAttributeForTextAreaElement(textAreaRef.current, true);
-          onSubmitReviewForm(id, formData);
-        }}
+        onSubmit={handleFormSubmit}
       >
 
         <div className="rating">
@@ -79,6 +57,7 @@ function ReviewForm({id, onSubmitReviewForm, postErrorMessage}) {
                   name="rating"
                   value={star}
                   onChange={handleInputChange}
+                  disabled={posting}
                 />
                 <label className="rating__label" htmlFor={`star-${star}`}>Rating {star}</label>
               </React.Fragment>
@@ -89,31 +68,31 @@ function ReviewForm({id, onSubmitReviewForm, postErrorMessage}) {
         <div className="add-review__text">
           <textarea
             className="add-review__textarea"
-            ref={textAreaRef}
-            name="review-text"
+            name="comment"
             id="review-text"
             placeholder="Review text"
             onChange={handleInputChange}
+            disabled={posting}
           />
           <div className="add-review__submit">
             <button
               className="add-review__btn"
               type="submit"
-              disabled={!isValidForm}
+              disabled={!isValidForm || posting}
             >
               Post
             </button>
           </div>
         </div>
       </form>
-      {postErrorMessage && <p>Something goes wrong, try again letter.</p>}
+      {errorMessage && <p>Something goes wrong, try again later.</p>}
     </div>
   );
 }
 
 ReviewForm.propTypes = {
   id: PropTypes.string.isRequired,
-  onSubmitReviewForm: PropTypes.func.isRequired,
-  postErrorMessage: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  errorMessage: PropTypes.string.isRequired,
 };
 export default ReviewForm;
