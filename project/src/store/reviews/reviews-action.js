642d42124df246da14a2d21d8abd156bd535110b
ildarjan <nobap84@rambler.ru>
2021-07-18 21:45:47 +0300

    remove addReviewStart

--- project/src/store/reviews/reviews-action.js
+++ project/src/store/reviews/reviews-action.js
@@ -2,7 +2,6 @@ import {ActionType} from '../action-type.js';
 import {createAction} from '@reduxjs/toolkit';
 
 export const fetchReviewsStart = createAction(ActionType.FETCH_REVIEWS_START);
-export const addReviewStart = createAction(ActionType.ADD_REVIEW_START);
 export const addReviewSuccess = createAction(ActionType.ADD_REVIEW_SUCCESS);
 
 export const fetchReviewsSuccess = createAction(
