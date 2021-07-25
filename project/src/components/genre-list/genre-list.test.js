ebdfa60f2f3509fa8bd3cdc55edebf153b4ad433
ildarjan <nobap84@rambler.ru>
2021-07-26 00:20:40 +0300

    refactor

--- project/src/components/genre-list/genre-list.test.js
+++ project/src/components/genre-list/genre-list.test.js
@@ -1,39 +1,33 @@
 import {Router} from 'react-router-dom';
-import {getByText, getNodeText, render} from '@testing-library/react';
+import {getNodeText, render} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import GenreList from './genre-list.jsx';
 import configureStore from 'redux-mock-store';
 import {Provider} from 'react-redux';
 
-let store;
-let history;
-let fakeApp;
 
 describe('Component: GenreList', () => {
-  beforeAll(() => {
-    history = createMemoryHistory();
+  it('Should render correctly', () => {
+    const history = createMemoryHistory();
     const createFakeStore = configureStore();
-    store = createFakeStore({
+    const store = createFakeStore({
       PROCESS: {currentGenre: 'All genres'},
       FILMS: {
         filmsByGenre: {'Drama': '', 'Comedy': '', 'Fantasy': '', 'Thriller': '', 'Crime': '', 'All genres': ''}
       }
     });
 
-    fakeApp = (
+    const {container} = render(
       <Provider store={store}>
         <Router history={history}>
           <GenreList/>
         </Router>
       </Provider>
     );
-  });
 
-  it('Should render correctly', () => {
-    const {container} = render(fakeApp);
-    const genreItems = container.querySelectorAll('li');
+    const genreItems = container.querySelectorAll('[data-testid="genre-item"]');
     const activeGenreItem = container.querySelector('.catalog__genres-item--active');
-    const activeGenreLink = container.querySelector('.catalog__genres-item--active > a');
+    const activeGenreLink = activeGenreItem.querySelector('a');
 
     expect(genreItems.length).toEqual(6);
     expect(activeGenreItem).toBeInTheDocument();
