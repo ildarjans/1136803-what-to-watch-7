5532fe8873464720e1651d4a5328e5686006a260
ildarjan <nobap84@rambler.ru>
2021-07-29 17:06:27 +0300

    rename VideoPlayer

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -4,7 +4,7 @@ import MainPage from '../main-page/main-page.jsx';
 import MoviePage from '../movie-page/movie-page.jsx';
 import MyList from '../my-list/my-list.jsx';
 import UserPage from '../user-page/user-page.jsx';
-import Player from '../player/player.jsx';
+import VideoPlayer from '../video-player/video-player.jsx';
 import PrivateRoute from '../private-route/private-route.jsx';
 import AddReviewPage from '../add-review-page/add-review-page.jsx';
 import NotFoundPage from '../not-found-page/not-found-page.jsx';
@@ -16,7 +16,7 @@ function App() {
       <Route exact path={AppRoute.ROOT} component={MainPage}/>
       <Route exact path={AppRoute.LOGIN} component={UserPage}/>
       <Route exact path={AppRoute.FILM} component={MoviePage}/>
-      <Route exact path={AppRoute.PLAYER} component={Player}/>
+      <Route exact path={AppRoute.PLAYER} component={VideoPlayer}/>
       <PrivateRoute exact path={AppRoute.REVIEW} render={() => <AddReviewPage/>}/>
       <PrivateRoute exact path={AppRoute.FAVORITES} render={() => <MyList/>}/>
       <Route component={NotFoundPage}/>
